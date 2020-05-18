c856986986cf0f60a8aa15768a56392bacc9bf70
M3156 <m3156@student.jamk.fi>
2020-05-18 19:13:48 +0300

    4.4 and 4.5 done

--- backend/utils/list_helper.js
+++ backend/utils/list_helper.js
@@ -2,6 +2,23 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  let likes = 0
+  blogs.forEach(blog => {
+    likes += blog.likes
+  });
+
+  return likes
+}
+
+const favoriteBlog = (blogs) => {
+  let favBlog = blogs.reduce((max, blog) => max.likes > blog.likes ? max : blog)
+
+  return favBlog
+}
+
 module.exports = {
   dummy,
+  totalLikes,
+  favoriteBlog,
 }
