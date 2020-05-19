b96e5632a2877fe6ad23d25bde96a7f7c9ec90a7
M3156 <m3156@student.jamk.fi>
2020-05-19 16:05:47 +0300

    4.6 done

--- backend/utils/list_helper.js
+++ backend/utils/list_helper.js
@@ -1,3 +1,5 @@
+const lodash = require('lodash')
+
 const dummy = (blogs) => {
   return 1
 }
@@ -17,8 +19,31 @@ const favoriteBlog = (blogs) => {
   return favBlog
 }
 
+const mostBlogs = (blogs) => {
+  const result = lodash.groupBy(blogs, 'author')
+
+  let len = 0
+  let auth = ''
+
+  for (var prop in result) {
+    if(Object.prototype.hasOwnProperty.call(result, prop)) {
+      if (result[prop].length > len) {
+        len = result[prop].length
+        auth = prop
+      }
+    }
+  }
+  const returnedObj = {
+    author: auth,
+    blogs: len
+  }
+
+  return returnedObj
+}
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
 }
