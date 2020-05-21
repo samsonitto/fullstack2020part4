37933d08fd20021998db97df0fb8b2d83091c499
M3156 <m3156@student.jamk.fi>
2020-05-21 15:58:26 +0300

    4.8 done

---
+++ backend/tests/blog_api.test.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('there are zero notes', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(0)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
