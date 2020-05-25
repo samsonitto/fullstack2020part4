38f17e3918ffa85857a3bc0b48ea993921d30eda
M3156 <m3156@student.jamk.fi>
2020-05-25 11:36:53 +0300

    4.18 done

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ const app = express()
 
 const blogRouter = require('./controllers/blogs')
 const userRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 
@@ -30,6 +31,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', userRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
