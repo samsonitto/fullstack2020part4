70d01afa9518a16ed63a967002dc17a0bd18e2c6
M3156 <m3156@student.jamk.fi>
2020-06-03 15:21:43 +0300

    5.14 done

--- frontend/src/tests/Blog.test.js
+++ frontend/src/tests/Blog.test.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from '../components/Blog'
 
-test('renders content', () => {
-  const blog = {
-    title: "Cat Science",
-    author: "Neil Degrasse Tyson",
-    url: "https://cats.com",
-    likes: 2,
-    date: "2020-05-27T13:53:40.041Z",
-    id: "5ec256fa0cf72c4ad0a3347a",
-  }
+const blog = {
+  title: "Cat Science",
+  author: "Neil Degrasse Tyson",
+  url: "https://cats.com",
+  likes: 2,
+  date: "2020-05-27T13:53:40.041Z",
+  id: "5ec256fa0cf72c4ad0a3347a",
+}
 
+test('renders content', () => {
   const component = render(
     <Blog blog={blog} />
   )
@@ -34,4 +34,23 @@ test('renders content', () => {
     '2'
   )
   expect(div).toHaveStyle('display: none')
+})
+
+test('renders hidden content', () => {
+  const component = render(
+    <Blog blog={blog} />
+  )
+
+  const button = component.getByText('View')
+  fireEvent.click(button)
+
+  const div = component.container.querySelector('.togglableContent')
+
+  expect(div).toHaveTextContent(
+    'https://cats.com'
+  )
+  expect(div).toHaveTextContent(
+    '2'
+  )
+  expect(div).not.toHaveStyle('display: none')
 })
