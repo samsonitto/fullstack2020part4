c0d54c5f31b0257232d986e238f549e92f0b054c
M3156 <m3156@student.jamk.fi>
2020-06-03 09:38:50 +0300

    5.12 fail

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  "env": {
+      "browser": true,
+      "es6": true
+  },
+  "extends": "airbnb",
+'globals': {
+  "Atomics": "readonly",
+      "SharedArrayBuffer": "readonly"
+  },
+  "parserOptions": {
+      "ecmaFeatures": {
+          "jsx": true
+      },
+      "ecmaVersion": 11
+  },
+  "plugins": [
+      "react"
+  ],
+  "rules": {
+      'linebreak-style': [
+          'error',
+          'windows'
+      ],
+      'semi': [
+          'error',
+          'never'
+      ]
+  }
+};
